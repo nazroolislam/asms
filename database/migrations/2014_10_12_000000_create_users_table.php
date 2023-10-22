b5aec36bbc56d44301eeeba02512143494be9bb9
nazroolislam <ni2796386@gmail.com>
2023-10-22 06:31:47 -0700

    user table model migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 128);
+            $table->string('email', 128)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email_verification_token', 64)->nullable();
+            $table->string('facebook_id', 32)->nullable();
+            $table->string('google_id', 32)->nullable();
+            $table->string('password', 128);
             $table->rememberToken();
             $table->timestamps();
         });
